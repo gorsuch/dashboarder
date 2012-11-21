9a497e7d4266b38ddce01c99a2c2e1ba14c432d4
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-11-21 11:49:20 -0600

    support for defining instruments by name only

--- lib/dashboarder/instrument.rb
+++ lib/dashboarder/instrument.rb
@@ -13,13 +13,20 @@ module Dashboarder
     end
     
     def self.compose(definition)
-      name = definition.first
-      instrument = get(name)
+      # if we passed an atom, attempt to fetch rather than build
+      if definition.kind_of?(String) || definition.kind_of?(Symbol)
+        instrument = get(definition)
+        raise "instrument #{definition} has not been created yet" unless instrument
+        instrument
+      else
+        name = definition.first
+        instrument = get(name)
 
-      unless instrument
-        instrument = compose!(definition)  
-      end
-      instrument
+        unless instrument
+          instrument = compose!(definition)  
+        end
+        instrument
+     end
     end
   end
 end
